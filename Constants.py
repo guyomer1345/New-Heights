304d20b1e4c8395dabd18669a2edf2e05d22164f
Guy <guyo0503@gmail.com>
2022-01-29 15:03:59 +0200

    changed anaconda to miniconda

--- Constants.py
+++ Constants.py
@@ -6,7 +6,7 @@ DOWNLOADS_DIR = os.path.join(WORK_DIR, "downloads") + os.path.sep
 INSTALLATIONS_DIR = os.path.join(WORK_DIR, "system", "installations") + os.path.sep
 
 SEVEN_ZIP_URL = "https://d3.7-zip.org/a/7z2107-x64.exe"
-ANACONDA_URL = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
+MINICONDA_URL = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
 
 SEVEN_ZIP_INSTALLATION_FOLDER = os.path.join(INSTALLATIONS_DIR, '7-Zip')
 SEVEN_ZIP_INSTALL_COMMAND = '%s /S /D={installation_folder}'.format(installation_folder=SEVEN_ZIP_INSTALLATION_FOLDER)
---
+++ minicondainstaller.py
@@ -0,0 +1,3 @@
+from Constants import ANACONDA_URL
+
+
