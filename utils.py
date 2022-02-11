ede806c62cc1eee0453a001027b266287c07516e
Lev <levblt1@gmail.com>
2022-02-11 14:42:52 +0200

    reformat file

--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
-from constants import DOWNLOADS_DIR, FILE_EXISTS, JSON_CONTENT_TYPE
 import json
-import time
-import requests
 import os
+import time
 from typing import Tuple, Dict
+
+import requests
+
+from constants import DOWNLOADS_DIR, FILE_EXISTS, JSON_CONTENT_TYPE
 
 
 def download_file(url):
@@ -43,15 +45,21 @@ def download_file_with_response(url: str) -> Tuple[str, int, Dict[str, str]]:
         file_time = os.path.getctime(DOWNLOADS_DIR + name)
 
         # TODO: change to proper version checking
-        if not ((time.time() - file_time) / 3600 > 24*30):
-            return json.dumps({'success':True, 'msg':str(name) + FILE_EXISTS}), 200, JSON_CONTENT_TYPE
+        if not ((time.time() - file_time) / 3600 > 24 * 30):
+            return json.dumps({'success': True,
+                               'msg': str(name) + FILE_EXISTS}), 200, \
+                   JSON_CONTENT_TYPE
 
     try:
         filename = download_file(url)
     except requests.exceptions.HTTPError as e:
-        return json.dumps({'error':False, 'msg':str(e), 'httpcode':e.response.status_code}), 500, JSON_CONTENT_TYPE
+        return json.dumps({'error': False, 'msg': str(e),
+                           'httpcode': e.response.status_code}), 500, \
+               JSON_CONTENT_TYPE
 
     if filename:
-        return json.dumps({'success':True, 'msg':str(filename)}), 200, JSON_CONTENT_TYPE
+        return json.dumps({'success': True, 'msg': str(filename)}), 200, \
+               JSON_CONTENT_TYPE
 
-    return json.dumps({'error':False, 'msg':str(filename)}), 500, JSON_CONTENT_TYPE
+    return json.dumps({'error': False, 'msg': str(filename)}), 500,  \
+           JSON_CONTENT_TYPE
