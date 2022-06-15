092d1e3cb70faba161c87b54d1beaacdbd505199
michaelbr92 <mbricman8@gmail.com>
2022-06-15 23:57:15 +0300

    Add vue apps

---
+++ www/js/apps/load.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container" id="loading" v-if="is_loading">
+        <div class="progress">
+            <div class="indeterminate"></div>
+        </div>
+    </div>
+    <div id="loading-bg"></div>
+</template>
+
+<script>
+export default {
+    name: "load-app",
+    template: "#load-app",
+    data() {
+        return {
+            is_loading: false,
+        };
+    }
+};
+</script>
+
+<style>
+#loading {
+    z-index: 100;
+}
+#loading-bg{
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    position: absolute;
+    z-index: 99;
+    background: rgba(255, 255, 255, 0.5);
+}
+</style>
---
+++ www/js/apps/nav.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="navbar-fixed">
+        <nav class="yellow lighten-1">
+            <div class="nav-wrapper">
+                <img src="logo.png" style="height: 100%" class="brand-logo left">
+                <ul class="right">
+                    <li><a href="#" @click="close"><i class="material-icons black-text">close</i></a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        M.AutoInit();
+    },
+    methods: {
+        close() {
+            pywebview.api.close()
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
